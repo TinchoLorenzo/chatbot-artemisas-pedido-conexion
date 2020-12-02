8233cbf69937751384a13fa3f05bc713e19078bf
mlorenzo <mlorenzo@alumnos.exa.unicen.edu.ar>
2020-12-02 08:49:28 -0300

    As

--- app.py
+++ app.py
@@ -4,25 +4,18 @@ import pandas as pd
 from datetime import datetime as dt
 import pika 
 
-data = pd.read_csv('data.csv', names=['s', 'e', 'm']).set_index('m')
-
-series = pd.Series(index=range(data.s.min(), dt.now().year + 1))
-for m in data.index:
-    series.loc[data.loc[m].s:data.loc[m].e] = m
-
 app = flask.Flask(__name__)
 
-
+connection = pika.BlockingConnection(pika.URLParameters("amqps://urfvnqok:kDPF6YteXqwoKytSirWyl_HAisUjTGYl@woodpecker.rmq.cloudamqp.com/urfvnqok"))
 @app.route('/', methods=['GET'])
 def home():
-    connection = pika.BlockingConnection(pika.URLParameters("amqps://urfvnqok:kDPF6YteXqwoKytSirWyl_HAisUjTGYl@woodpecker.rmq.cloudamqp.com/urfvnqok"))
     channel = connection.channel()
     #channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
 
     routing_key = "Chatbot.PedidoConeccion"
     message = '{ "url": https://botdisenio.herokuapp.com/webhooks/my_connector/webhook/ }'
     channel.basic_publish(exchange='topic_logs', routing_key=routing_key, body=message)
-    connection.close()
+    #connection.close()
     year = int(request.args['year'])
     try:
         return series.loc[year]
