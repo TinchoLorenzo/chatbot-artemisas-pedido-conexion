6264120eed5f79dd0eeb8f1f732ca21143aa89fa
mlorenzo <mlorenzo@alumnos.exa.unicen.edu.ar>
2020-12-03 16:34:20 -0300

    finalizando

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import request
 #import pandas as pd
 from datetime import datetime as dt
 import pika 
+import json 
 
 app = flask.Flask(__name__)
 
@@ -12,9 +13,9 @@ def home():
     connection = pika.BlockingConnection(pika.URLParameters("amqps://urfvnqok:kDPF6YteXqwoKytSirWyl_HAisUjTGYl@woodpecker.rmq.cloudamqp.com/urfvnqok"))
     channel = connection.channel()
     #channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
-    
+    url = json.loads(request.json)['url']
     routing_key = "Chatbot.PedidoConeccion"
-    message = '{"url": ' + request.json['url'] + ' }'
+    message = '{"url": {0} }'.format(url)
     channel.basic_publish(exchange='topic_logs', routing_key=routing_key, body=message)
     connection.close()
     return "Hello world"
