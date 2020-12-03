9d1c4caaf4fe705e7ae480439b91baed5e96bd20
mlorenzo <mlorenzo@alumnos.exa.unicen.edu.ar>
2020-12-03 16:18:57 -0300

    Finish

--- app.py
+++ app.py
@@ -7,16 +7,15 @@ import pika
 app = flask.Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def home():
     connection = pika.BlockingConnection(pika.URLParameters("amqps://urfvnqok:kDPF6YteXqwoKytSirWyl_HAisUjTGYl@woodpecker.rmq.cloudamqp.com/urfvnqok"))
     channel = connection.channel()
     #channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
-
+    
     routing_key = "Chatbot.PedidoConeccion"
-    message = '{"url": "https://botdisenio.herokuapp.com/webhooks/my_connector/webhook/" }'
+    message = '{"url": ' + request.args['url'] + ' }'
     channel.basic_publish(exchange='topic_logs', routing_key=routing_key, body=message)
     connection.close()
-    #year = int(request.args['year'])
     return "Hello world"
     
